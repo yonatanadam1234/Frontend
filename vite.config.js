af1d484b891cb70d0c33ebd06a79b48060041b58
jbh105 <jaydipbhayani@gmail.com>
2024-04-26 10:11:06 +0530

    fixed the build issues

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Listen on all IPv4 addresses
+  },
   build: {
-    outDir: 'build', 
+    outDir: 'build',
   },
   resolve: {
     alias: [{ find: "@crema", replacement: "/src/@crema" }],
