af7c486f4fe3b781068009ce99ac9827d70e750b
jbh105 <jaydipbhayani105@gmail.com>
2024-05-08 09:46:23 +0530

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0', // Listen on all IPv4 addresses
+    host: 'localhost',
   },
   build: {
     outDir: 'build',
