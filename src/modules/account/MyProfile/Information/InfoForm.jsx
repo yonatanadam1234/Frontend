7056dcb3f0195a961fd341c9e4c76651e5ce413d
JBH105 <jaydipbhayani105@gmail.com>
2024-07-30 15:32:49 +0530

    changes in plan

--- src/modules/account/MyProfile/Information/InfoForm.jsx
+++ src/modules/account/MyProfile/Information/InfoForm.jsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
-import { Form } from 'formik';
-import PropTypes from 'prop-types';
-import { Button } from '@mui/base';
-import { Box, Grid, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useJWTAuth, useJWTAuthActions } from '../../../../@crema/services/auth';
-import { pricingPlanData } from '../../../extraPages/Pricing/PackageOneNew/PackageCard/Services/pricing.service';
-import jwtAxios from '../../../../@crema/services/auth/jwt-auth';
-import AppLoader from '../../../../@crema/components/AppLoader';
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { Form } from "formik";
+import PropTypes from "prop-types";
+import { Button } from "@mui/base";
+import { Box, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import {
+  useJWTAuth,
+  useJWTAuthActions,
+} from "../../../../@crema/services/auth";
+import { pricingPlanData } from "../../../extraPages/Pricing/PackageOneNew/PackageCard/Services/pricing.service";
+import jwtAxios from "../../../../@crema/services/auth/jwt-auth";
+import AppLoader from "../../../../@crema/components/AppLoader";
 
 const InfoForm = () => {
   const { user } = useJWTAuth();
@@ -17,14 +20,19 @@ const InfoForm = () => {
   const navigate = useNavigate();
 
   const handleUpgrade = () => {
-    navigate('/extra-pages/pricing-detail');
+    navigate("/extra-pages/pricing-detail");
   };
 
   const fetchPricingData = useCallback(async () => {
     setLoading(true);
     try {
       const response = await pricingPlanData();
-      if (response && response.data && response.data.data && response.data.data.plans) {
+      if (
+        response &&
+        response.data &&
+        response.data.data &&
+        response.data.data.plans
+      ) {
         setPricingPlans(response.data.data.plans);
       } else {
         console.error("Invalid pricing data response");
@@ -41,10 +49,10 @@ const InfoForm = () => {
   }, []);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       jwtAxios
-        .get('auth/user-data', {
+        .get("auth/user-data", {
           headers: { Authorization: `Bearer ${token}` },
         })
         .then((data) => {
@@ -72,51 +80,69 @@ const InfoForm = () => {
     return planById;
   }, [pricingPlans]);
 
-  const currentPlan = useMemo(() => getPlanById(user?.subscription), [user?.subscription, pricingPlans]);
+  const currentPlan = useMemo(
+    () => getPlanById(user?.subscription),
+    [user?.subscription, pricingPlans]
+  );
+  console.log("🚀 ~ InfoForm ~ currentPlan:", currentPlan);
 
   return (
-    <Form autoComplete='off'>
-      <Typography sx={{my:3,fontSize:'20px'}}>Subscription Plan</Typography>
-      
+    <Form autoComplete="off">
+      <Typography sx={{ my: 3, fontSize: "20px" }}>
+        Subscription Plan
+      </Typography>
+
       {loading ? (
-        <Box sx={{py:'100px'}}>
+        <Box sx={{ py: "100px" }}>
           <AppLoader />
         </Box>
       ) : (
         <>
-        <Typography>Your current plan: {currentPlan ? currentPlan.tag : 'Unknown'}</Typography>
-      <br />
-      {currentPlan && (
-        <div>
-          <h2>{currentPlan.title}</h2>
-          <p>Price: ${currentPlan.payment_price}</p>
-          <p>Duration: {currentPlan.payment_interval_unit}</p>
-        </div>
-      )}
+          <Typography>
+            Your current plan: {currentPlan ? currentPlan.tag : "Unknown"}
+          </Typography>
+          <br />
+          {currentPlan && (
+            <div>
+              <h2>{currentPlan.title}</h2>
+              <p>Price: ${currentPlan.payment_price}</p>
+              {currentPlan.id !== 1 && currentPlan.id !== 2 && (
+                <p>Duration: {currentPlan.payment_interval_unit}</p>
+              )}
+            </div>
+          )}
 
-      {currentPlan?.id !== 8 && (
-        <Grid item xs={12} md={12}>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            <Button
-              style={{ padding: '10px', borderRadius: '8%', margin: '15px 0px', background: '#0A8FDC', border: 'none', color: '#fff', fontSize: '14px', cursor: 'pointer' }}
-              color='primary'
-              variant='contained'
-              type='submit'
-              onClick={handleUpgrade}
-            >
-              Upgrade Plan
-            </Button>
-          </Box>
-        </Grid>
+          {currentPlan?.id !== 8 && (
+            <Grid item xs={12} md={12}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  style={{
+                    padding: "10px",
+                    borderRadius: "8%",
+                    margin: "15px 0px",
+                    background: "#0A8FDC",
+                    border: "none",
+                    color: "#fff",
+                    fontSize: "14px",
+                    cursor: "pointer",
+                  }}
+                  color="primary"
+                  variant="contained"
+                  type="submit"
+                  onClick={handleUpgrade}
+                >
+                  Upgrade Plan
+                </Button>
+              </Box>
+            </Grid>
+          )}
+        </>
       )}
-      </>
-    )}
-
     </Form>
   );
 };
--- src/modules/extraPages/Pricing/PackageOneNew/PackageCard/index.jsx
+++ src/modules/extraPages/Pricing/PackageOneNew/PackageCard/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Typography, Box, Button, List, ListItem, ListItemIcon } from "@mui/material";
+import {
+  Typography,
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemIcon,
+} from "@mui/material";
 import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
 import { Fonts } from "@crema/constants/AppEnums";
 import { FaBoxOpen } from "react-icons/fa";
 import { BiSolidShoppingBagAlt } from "react-icons/bi";
-import { useJWTAuth, useJWTAuthActions } from "../../../../../@crema/services/auth";
+import {
+  useJWTAuth,
+  useJWTAuthActions,
+} from "../../../../../@crema/services/auth";
 import CardWrapper from "./CardWrapper";
 import PackageWrapper from "./PackageWrapper";
 import jwtAxios from "../../../../../@crema/services/auth/jwt-auth";
@@ -18,7 +28,7 @@ const PackageCard = ({ pricing, buttonText, billingFormat }) => {
 
   Paddle.Environment.set("production");
   Paddle.Initialize({
-    token: 'live_1bc6cf442aa74adbab7ffae494d', 
+    token: "live_1bc6cf442aa74adbab7ffae494d",
     pwCustomer: {},
     eventCallback: function (data) {
       console.log("🚀 ~ PackageCard ~ data:", data);
@@ -28,43 +38,43 @@ const PackageCard = ({ pricing, buttonText, billingFormat }) => {
           fetchUpdateUser();
         });
       }
-    }
+    },
   });
 
   const openCheckout = (priceId) => {
     Paddle.Checkout.open({
       items: [{ priceId: priceId, quantity: 1 }],
-      customData: { "any_profit_user_id": user.id }
+      customData: { any_profit_user_id: user.id },
     });
   };
 
   const getSubscriptionId = (priceId) => {
     const priceIdMap = {
-      'pri_01hzey153328nc96wa7xcpja1t': 3,
-      'pri_01hzey2t6yyvpc1148620mft19': 5,
-      'pri_01hzey58skrkf53ybt98s3j6y4': 7,
-      'pri_01hzey22rjs4jsqb357drc7nnf': 4,
-      'pri_01hzey3gxntw4ck5wgvhdx6bkx': 6,
-      'pri_01hzey4cb2r0gzts3y1n3kc5xr': 8,
+      pri_01hzey153328nc96wa7xcpja1t: 3,
+      pri_01hzey2t6yyvpc1148620mft19: 5,
+      pri_01hzey58skrkf53ybt98s3j6y4: 7,
+      pri_01hzey22rjs4jsqb357drc7nnf: 4,
+      pri_01hzey3gxntw4ck5wgvhdx6bkx: 6,
+      pri_01hzey4cb2r0gzts3y1n3kc5xr: 8,
     };
     return priceIdMap[priceId] || null;
   };
 
   const handleSuccess = async (data) => {
     try {
-      console.log('Paddle Checkout Success:', data);
+      console.log("Paddle Checkout Success:", data);
       const priceId = data.data.items[0].price_id;
       console.log("Price ID:", priceId);
       const subscriptionId = getSubscriptionId(priceId);
 
       if (subscriptionId) {
         const response = await updateSubscription(subscriptionId);
-        console.log('Subscription updated successfully:', response);
+        console.log("Subscription updated successfully:", response);
       } else {
-        console.error('Invalid priceId:', priceId);
+        console.error("Invalid priceId:", priceId);
       }
     } catch (error) {
-      console.error('Error handling Paddle Checkout success:', error);
+      console.error("Error handling Paddle Checkout success:", error);
     }
   };
 
@@ -74,20 +84,22 @@ const PackageCard = ({ pricing, buttonText, billingFormat }) => {
   };
 
   const fetchUpdateUser = () => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     jwtAxios
-      .get('auth/user-data', {
+      .get("auth/user-data", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      }).then((data) => {
+      })
+      .then((data) => {
         console.log("Updated user data:", data);
         setJWTAuthData({
           user: data.data.user,
           isLoading: false,
           isAuthenticated: true,
         });
-      }).catch(error => {
+      })
+      .catch((error) => {
         console.error("Error updating user data:", error);
       });
   };
@@ -95,37 +107,67 @@ const PackageCard = ({ pricing, buttonText, billingFormat }) => {
   return (
     <>
       <PackageWrapper>
-        <Box component="span" className="tag" sx={{ backgroundColor: pricing.color }}>
+        <Box
+          component="span"
+          className="tag"
+          sx={{ backgroundColor: pricing.color }}
+        >
           {pricing.tag}
         </Box>
         <CardWrapper>
           <Box sx={{ position: "relative", pr: 20 }}>
-            <Typography component="h3" sx={{ fontWeight: Fonts.BOLD, fontSize: { xs: 28, md: 32, lg: 36 } }}>
+            <Typography
+              component="h3"
+              sx={{
+                fontWeight: Fonts.BOLD,
+                fontSize: { xs: 28, md: 32, lg: 36 },
+              }}
+            >
               {pricing.title}
             </Typography>
-            <Typography component="h4" sx={{ fontSize: { xs: 20, md: 22, lg: 24 }, mb: { xs: 5, lg: 7.5 } }}>
+            <Typography
+              component="h4"
+              sx={{
+                fontSize: { xs: 20, md: 22, lg: 24 },
+                mb: { xs: 5, lg: 7.5 },
+              }}
+            >
               <Box component="span" sx={{ fontWeight: Fonts.BOLD }}>
                 ${pricing.payment_price}
               </Box>
               /{billingFormat}
             </Typography>
           </Box>
-          <Box sx={{ minHeight: 40, display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ minHeight: 40, display: "flex", flexDirection: "column" }}>
             <ListItemIcon sx={{ minWidth: 10, mr: 2.5 }}>
-              <FaBoxOpen style={{ fontSize: "18px" }} sx={{ mt: 1, mb: 3.5, color: (theme) => theme.palette.text.primary }} />
+              <FaBoxOpen
+                style={{ fontSize: "18px" }}
+                sx={{
+                  mt: 1,
+                  mb: 3.5,
+                  color: (theme) => theme.palette.text.primary,
+                }}
+              />
               &nbsp;{pricing.quotas.order_title}
             </ListItemIcon>
 
             {pricing.quotas.shop ? (
               <ListItemIcon sx={{ minWidth: 10, mr: 2.5 }}>
-                <BiSolidShoppingBagAlt style={{ fontSize: "18px" }} sx={{ mt: 0, mb: 3.5, color: (theme) => theme.palette.text.primary }} />
+                <BiSolidShoppingBagAlt
+                  style={{ fontSize: "18px" }}
+                  sx={{
+                    mt: 0,
+                    mb: 3.5,
+                    color: (theme) => theme.palette.text.primary,
+                  }}
+                />
                 &nbsp;Up to {pricing.quotas.shop} Platform
               </ListItemIcon>
             ) : null}
           </Box>
 
-          {buttonText === 'Current Plan' || buttonText === 'Free' ? (
-            <Box sx={{ mb: 7.5, mt: 7 }}>
+          {buttonText === "Current Plan" || buttonText === "Free" ? (
+            <Box sx={{ mb: 7.5, mt: buttonText === "Free" ? 3 : 7 }}>
               <Button
                 variant="outlined"
                 sx={{
@@ -137,7 +179,10 @@ const PackageCard = ({ pricing, buttonText, billingFormat }) => {
                   boxShadow: "none",
                   borderWidth: 2,
                   borderColor: pricing.color,
-                  "&:hover, &:focus": { borderColor: pricing.color, borderWidth: 2 },
+                  "&:hover, &:focus": {
+                    borderColor: pricing.color,
+                    borderWidth: 2,
+                  },
                 }}
                 onClick={handleButtonClick}
                 disabled
@@ -158,7 +203,10 @@ const PackageCard = ({ pricing, buttonText, billingFormat }) => {
                   boxShadow: "none",
                   borderWidth: 2,
                   borderColor: pricing.color,
-                  "&:hover, &:focus": { borderColor: pricing.color, borderWidth: 2 },
+                  "&:hover, &:focus": {
+                    borderColor: pricing.color,
+                    borderWidth: 2,
+                  },
                 }}
                 onClick={handleButtonClick}
               >
@@ -167,18 +215,46 @@ const PackageCard = ({ pricing, buttonText, billingFormat }) => {
             </Box>
           )}
 
-          <Box>Extra Order fee: ${pricing.quotas.extra_order_fee} per order</Box>
+          <Box>
+            Extra Order fee: ${pricing.quotas.extra_order_fee} per order
+          </Box>
           <hr color="#0A8FDC" style={{ marginTop: "30px" }} />
           {JSON.parse(pricing.quotas.services).map((data, index) => (
             <List key={index}>
               <ListItem sx={{ display: "block", m: 0 }}>
-                <Box sx={{ color: "#0A8FDC", mb: 5, fontSize: "16px", fontWeight: 1000 }}>{data.title}</Box>
+                <Box
+                  sx={{
+                    color: "#0A8FDC",
+                    mb: 5,
+                    fontSize: "16px",
+                    fontWeight: 1000,
+                  }}
+                >
+                  {data.title}
+                </Box>
                 {data.allData.map((item) => (
-                  <Box key={item.id} sx={{ display: "flex", alignItems: "center", ml: -3, p: -7 }}>
+                  <Box
+                    key={item.id}
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      ml: -3,
+                      p: -7,
+                    }}
+                  >
                     <ListItemIcon sx={{ minWidth: 10, mr: 2.5 }}>
-                      <CheckOutlinedIcon sx={{ fontSize: 16, mt: 1, mb: 3.5, color: (theme) => theme.palette.text.primary }} />
+                      <CheckOutlinedIcon
+                        sx={{
+                          fontSize: 16,
+                          mt: 1,
+                          mb: 3.5,
+                          color: (theme) => theme.palette.text.primary,
+                        }}
+                      />
                     </ListItemIcon>
-                    <Box sx={{ display: "block", width: "100%", mb: 3.5 }}>{item.title}</Box>
+                    <Box sx={{ display: "block", width: "100%", mb: 3.5 }}>
+                      {item.title}
+                    </Box>
                   </Box>
                 ))}
               </ListItem>
